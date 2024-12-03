e9b3a9a79f8c2723a26801e29ec28628f8b97a3d
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-12-03 16:23:14 +0800

    chore: allow editing submission on some submission status

--- app/Policies/SubmissionPolicy.php
+++ app/Policies/SubmissionPolicy.php
@@ -355,14 +355,6 @@ class SubmissionPolicy
 
     public function editing(User $user, Submission $submission)
     {
-        if ($submission->stage != SubmissionStage::Editing) {
-            return false;
-        }
-
-        if (filled($submission->withdrawn_reason)) {
-            return false;
-        }
-
         if (in_array($submission->status, [SubmissionStatus::Published, SubmissionStatus::Declined, SubmissionStatus::Withdrawn])) {
             return false;
         }
