06df5fccbe6a9c153390b3d5d02522f575719401
Rahman Ramsi <rahmanramsi19@gmail.com>
2025-02-19 08:14:46 +0800

    chore: bump version

--- app/Application.php
+++ app/Application.php
@@ -37,7 +37,7 @@ use Illuminate\Support\Collection;
 
 class Application extends LaravelApplication
 {
-    public const APP_VERSION = '1.2.0';
+    public const APP_VERSION = '1.2.1';
 
     public const PHP_MIN_VERSION = '8.1';
 
