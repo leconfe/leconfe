df545298f944ccd67c7c1a363dcb80ffaa1c6e25
Rahman Ramsi <rahmanramsi19@gmail.com>
2025-04-11 08:48:37 +0800

    chore:  prepare version 1.2.5

--- app/Application.php
+++ app/Application.php
@@ -37,7 +37,7 @@ use Illuminate\Support\Collection;
 
 class Application extends LaravelApplication
 {
-    public const APP_VERSION = '1.2.4';
+    public const APP_VERSION = '1.2.5';
 
     public const PHP_MIN_VERSION = '8.1';
 
