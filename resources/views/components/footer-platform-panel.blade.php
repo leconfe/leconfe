c3fcab69705b643dcf9c48724eb6da270efaa8ac
Rahman Ramsi <rahmanramsi19@gmail.com>
2025-06-10 10:42:34 +0800

    chore: change footer

--- resources/views/components/footer-platform-panel.blade.php
+++ resources/views/components/footer-platform-panel.blade.php
@@ -2,7 +2,7 @@
 	<div class="mx-auto p-2 flex">
 		<a href="https://leconfe.com" rel="external" target="_blank" class="mx-auto flex items-center text-sm gap-x-2">
 			<img src="{{ Vite::asset('resources/assets/images/logo.png') }}" class="h-4" alt="leconfe-logo-footer">
-			<span class="font-medium text-gray-500">Leconfe : a simplify conference platform</span>
+			<span class="font-medium text-gray-500">Leconfe : Complete Academic Conference Platform</span>
 		</a>
 	</div>
 </div>
