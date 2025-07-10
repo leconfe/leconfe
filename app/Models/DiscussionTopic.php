f237966f6154f67278088dd5e13494db6e2d1717
Rahman Ramsi <rahmanramsi19@gmail.com>
2025-07-10 11:05:15 +0800

    fix: error cannot delete submission

--- app/Models/DiscussionTopic.php
+++ app/Models/DiscussionTopic.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 
 class DiscussionTopic extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'stage',
