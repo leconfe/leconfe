69e951e5734e41b40dbfe2cc6a295c89ea3cc140
Rahman Ramsi <rahmanramsi19@gmail.com>
2025-07-20 21:47:24 +0800

    add select files when sending to review

--- app/Panel/ScheduledConference/Livewire/Submissions/CallforAbstract.php
+++ app/Panel/ScheduledConference/Livewire/Submissions/CallforAbstract.php
@@ -2,12 +2,14 @@
 
 namespace App\Panel\ScheduledConference\Livewire\Submissions;
 
+use App\Constants\SubmissionFileCategory;
 use App\Forms\Components\TinyEditor;
 use App\Mail\Templates\AcceptAbstractMail;
 use App\Mail\Templates\DeclineAbstractMail;
 use App\Models\DefaultMailTemplate;
 use App\Models\Enums\SubmissionStatus;
 use App\Models\Submission;
+use App\Models\SubmissionFile;
 use App\Notifications\AbstractAccepted;
 use App\Notifications\AbstractDeclined;
 use App\Panel\ScheduledConference\Resources\SubmissionResource;
@@ -15,12 +17,14 @@ use Filament\Actions\Action;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
 use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Form;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\HtmlString;
 use Livewire\Component;
 
 class CallforAbstract extends Component implements HasActions, HasForms
@@ -147,6 +151,38 @@ class CallforAbstract extends Component implements HasActions, HasForms
                 ]);
             })
             ->form([
+                CheckboxList::make('papers')
+                    ->label('Select files below to send them to the review stage')
+                    ->hidden(
+                        ! $this->submission->getMedia(SubmissionFileCategory::ABSTRACT_FILES)->count()
+                    )
+                    ->options(function () {
+                        return $this->submission
+                            ->submissionFiles()
+                            ->with(['media'])
+                            ->where('category', SubmissionFileCategory::ABSTRACT_FILES)
+                            ->get()
+                            ->mapWithKeys(function (SubmissionFile $paper) {
+                                return [
+                                    $paper->getKey() => new HtmlString(
+                                        Action::make($paper->media->file_name)
+                                            ->label($paper->media->file_name)
+                                            ->url(fn () => $paper->media->getTemporaryUrl(now()->addMinutes(5)))
+                                            ->link()
+                                            ->toHtml()
+                                    ),
+                                ];
+                            });
+                    })
+                    ->descriptions(function () {
+                        return $this->submission
+                            ->submissionFiles()
+                            ->where('category', SubmissionFileCategory::ABSTRACT_FILES)
+                            ->get()
+                            ->mapWithKeys(function (SubmissionFile $paper) {
+                                return [$paper->getKey() => $paper->type->name];
+                            });
+                    }),
                 Fieldset::make('Notification')
                     ->label(__('general.notification'))
                     ->columns(1)
@@ -179,6 +215,27 @@ class CallforAbstract extends Component implements HasActions, HasForms
                 function (Action $action, array $data) {
                     try {
                         $this->submission->state()->acceptAbstract();
+
+                        $submissionFiles = $this->submission
+                            ->submissionFiles()
+                            ->with(['media'])
+                            ->whereIn('id', $data['papers'])
+                            ->get();
+
+                        foreach ($submissionFiles as $record) {
+                            $clonedMedia = $record->media->copy(
+                                $this->submission,
+                                SubmissionFileCategory::PAPER_FILES,
+                                'private-files'
+                            );
+                            $this->submission
+                                ->submissionFiles()
+                                ->create([
+                                    'submission_file_type_id' => $record->type->getKey(),
+                                    'category' => SubmissionFileCategory::PAPER_FILES,
+                                    'media_id' => $clonedMedia->getKey(),
+                                ]);
+                        }
 
                         if (! $data['no-notification']) {
                             try {
