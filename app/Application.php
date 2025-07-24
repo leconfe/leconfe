ce3319999bac993c3c1fbab916e3a3a1a3137e77
Rahman Ramsi <rahmanramsi19@gmail.com>
2025-07-24 09:13:34 +0800

    chore: bump version to 1.3.0-beta.3

--- app/Application.php
+++ app/Application.php
@@ -39,7 +39,7 @@ use Illuminate\Support\Collection;
 
 class Application extends LaravelApplication
 {
-    public const APP_VERSION = '1.3.0-beta.2';
+    public const APP_VERSION = '1.3.0-beta.3';
 
     public const PHP_MIN_VERSION = '8.1';
 
