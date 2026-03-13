3fdef87f0892079d3faed7292396f7177ed60afa
Rahman Ramsi <rahmanramsi19@gmail.com>
2026-03-13 10:06:56 +0800

    chore: improve review page

--- resources/views/panel/conference/resources/submission-resource/pages/review-submission-page.blade.php
+++ resources/views/panel/conference/resources/submission-resource/pages/review-submission-page.blade.php
@@ -1,5 +1,12 @@
+@php
+    $hasGuidanceModalContent = $this->hasGuidanceModalContent();
+    $hasReviewGuidelines = $this->hasReviewGuidelines();
+    $hasCompetingInterests = $this->hasCompetingInterests();
+@endphp
+
 <x-filament-panels::page x-on:show-guidelines="$dispatch('open-modal', {'id': 'guidelines'})"  x-data="
   {  
+    hasGuidanceModalContent: {{ $hasGuidanceModalContent ? 'true' : 'false' }},
     autoShowGuidelinesEnable() {
         return localStorage.getItem('autoShowGuidelines') != 0;
     },
@@ -7,7 +14,7 @@
         localStorage.setItem('autoShowGuidelines', this.autoShowGuidelinesEnable() ? 0 : 1);
     },
     init() {
-        if(this.autoShowGuidelinesEnable()) {
+        if(this.hasGuidanceModalContent && this.autoShowGuidelinesEnable()) {
             $nextTick(() => {
                 $dispatch('open-modal', {'id': 'guidelines'})
             });
@@ -17,7 +24,7 @@
     ">
     @if($review->reviewSubmitted())
         <x-shout::shout type="success" color="success" icon="heroicon-o-check-circle">
-            Thank you for your time and effort in reviewing this submission. Your review will be used to help the editor make a decision on the submission.
+            {{ __('general.review_thank_you_message') }}
         </x-shout::shout>
     @endif
     <div class="grid grid-cols-12 gap-4">
@@ -42,41 +49,47 @@
             {{ $this->infolist }}
         </div>
     </div>
+    @if($hasGuidanceModalContent)
     <x-filament::modal id="guidelines" width="2xl" :close-by-clicking-away="false" >
         <x-slot name="heading">
             <h1 class="text-xl font-bold">
-                Review Guidlines & Competing Interests
+                {{ __('general.review_guidelines_and_competing_interests') }}
             </h1>
         </x-slot>
         <x-slot name="description">
-            Please read the following guidelines and competing interests before submitting your review.
+            {{ __('general.review_guidelines_modal_description') }}
         </x-slot>
         <div class="flex flex-col space-y-4">
+            @if($hasReviewGuidelines)
             <div>
                 <h2 class="text-lg font-bold">
-                    Review Guidelines
+                    {{ __('general.review_guidelines') }}
                 </h2>
                 {!! $currentScheduledConference->getMeta('review_guidelines') !!}
             </div>
+            @endif
+            @if($hasCompetingInterests)
             <div>
                 <h2 class="text-lg font-bold">
-                    Competing Interests
+                    {{ __('general.competing_interests') }}
                 </h2>
                 {!! $currentScheduledConference->getMeta('competing_interests') !!}
             </div>
+            @endif
 
             <div class="flex items-center mb-4">
                 <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 rounded focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" :checked="!autoShowGuidelinesEnable()" x-on:change="toggleAutoShowGuidelines()">
                 <label for="default-checkbox" class="ms-2 text-sm text-gray-900 dark:text-gray-300">
-                    Understood guidelines and interests. Don't show this again
+                    {{ __('general.review_guidelines_understood') }}
                 </label>
             </div>
 
             <x-slot name="footerActions">
                 <x-filament::button color="gray" x-on:click="$dispatch('close-modal', {'id': 'guidelines'})">
-                    Close
+                    {{ __('general.close') }}
                 </x-filament::button>
             </x-slot>
         </div>
     </x-filament::modal>
+    @endif
 </x-filament-panels::page>
