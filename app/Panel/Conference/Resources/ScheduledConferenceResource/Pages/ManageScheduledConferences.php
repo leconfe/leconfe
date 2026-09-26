e0d87d57bcaa6d02ff48215a8112e54fe1429501
Rahman Ramsi <rahmanramsi19@gmail.com>
2026-09-26 08:58:50 +0800

    Redirect new cloud conferences to their panel

--- app/Panel/Conference/Resources/ScheduledConferenceResource/Pages/ManageScheduledConferences.php
+++ app/Panel/Conference/Resources/ScheduledConferenceResource/Pages/ManageScheduledConferences.php
@@ -3,6 +3,7 @@
 namespace App\Panel\Conference\Resources\ScheduledConferenceResource\Pages;
 
 use App\Actions\ScheduledConferences\ScheduledConferenceCreateAction;
+use App\Models\ScheduledConference;
 use App\Panel\Conference\Resources\ScheduledConferenceResource;
 use Filament\Actions;
 use Filament\Resources\Components\Tab;
@@ -19,7 +20,8 @@ class ManageScheduledConferences extends ManageRecords
         return [
             Actions\CreateAction::make()
                 ->modalWidth(MaxWidth::ExtraLarge)
-                ->using(fn (array $data) => ScheduledConferenceCreateAction::run($data)),
+                ->using(fn (array $data) => ScheduledConferenceCreateAction::run($data))
+                ->successRedirectUrl(fn (ScheduledConference $record): ?string => config('app.cloud', false) ? $record->getPanelUrl() : null),
         ];
     }
 
@@ -27,7 +29,7 @@ class ManageScheduledConferences extends ManageRecords
     {
         return [
             'all' => Tab::make()
-                ->label("All")
+                ->label('All')
                 ->badge(fn () => ScheduledConferenceResource::getEloquentQuery()->count()),
             'trash' => Tab::make()
                 ->label(__('general.trash'))
